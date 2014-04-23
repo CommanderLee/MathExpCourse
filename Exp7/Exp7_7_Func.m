69b15032dedac3be7716ceb32dd21b7af56b6325
LiZhen <dao.yi.er.san@gmail.com>
2014-04-23 14:30:04 +0800

    Finish Exp7.7's non-linear version

---
+++ Exp7/Exp7_7_Func.m
@@ -0,0 +1,13 @@
+% Function of Exp7.7, Page 171. Li Zhen, Apr. 23rd, 2014.
+
+function out = Exp7_7_Func(x, KL)
+a = x(1);
+alpha = x(2);
+beta = x(3);
+
+K = KL(:, 1);
+L = KL(:, 2);
+
+out = a .* (K .^ alpha) .* (L .^ beta);
+
+end
